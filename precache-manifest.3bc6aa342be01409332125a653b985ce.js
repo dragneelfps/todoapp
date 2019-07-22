c95eb2d7a51678a38a28828dde159ff40ce0084f
Sourabh S. Rawat <dragneelfps@gmail.com>
2019-07-22 22:06:31 +0530

    Updates

---
+++ precache-manifest.3bc6aa342be01409332125a653b985ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87714cac23367e4be77b8f49795b1d05",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ce84d8c80c6ff09acbb9",
+    "url": "/todoapp/static/css/main.4d24db4e.chunk.css"
+  },
+  {
+    "revision": "31ae52982c22838df05b",
+    "url": "/todoapp/static/js/2.63e2bf38.chunk.js"
+  },
+  {
+    "revision": "ce84d8c80c6ff09acbb9",
+    "url": "/todoapp/static/js/main.9b820201.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
